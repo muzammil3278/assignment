227a4b0bf2cfe8074ee246f7a3f6382a1d8517fa
muzammil3278 <mmuzammil528982@gmail.com>
2022-12-09 20:56:05 +0500

    km

--- pages/assesment-1/index.js
+++ pages/assesment-1/index.js
@@ -1,38 +1,50 @@
-import React from 'react'
+import React from "react";
 
 export default function index() {
   return (
     <div>
-        
-        <h1>HTML Assignment 1 Batch 6</h1>
-    <h1>Heading 1</h1>
-    <p>Heading 1 is made by using H1 tag</p>
-    <h2>Heading 2</h2>
-    <p>Heading 2 is made by using H2 tag</p>
-    <h3>Heading 3</h3>
-    <p>Heading 3 is made by using H3 tag</p>
-    <h4>Heading 4</h4>
-    <p>Heading 4 is made by using H4 tag</p>
-    <h5>Heading 5</h5>
-    <p>Heading 5 is made by using H5 tag</p>
-    <h6>Heading 6</h6>
-    <p>Heading 6 is made by using H6 tag</p>
-    <h1><u>Paragraph</u></h1>
-    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Esse, magnam dolorem debitis odio a tempora, quibusdam
-        fugiat quae ad voluptates et facilis odit nisi ipsa placeat eaque ex quos harum consectetur quis? Praesentium
-        reprehenderit totam, atque soluta, natus mollitia temporibus ducimus quisquam repellat earum consequatur
-          vitae ratione quisquam eligendi repellat porro, quas dicta quos necessitatibus reiciendis illum quod
-                quo. Error quidem quisquam dolorum nostrum nemo rerum facere eius, modi doloremque, officiis iste natus
-                consequuntur. Molestias voluptas quasi accusamus quisquam. Pariatur, quaerat? </i></b></u>Architecto sint
-                aliaslaboriosam eum quod autem molestias explicabo ab repellat quas ducimus ut, sapiente
-    </p>
-    <b>Bold Text</b><i>Italic Text</i><u>Underline Text</u>
-    <h1><b>How to Bold any Text?</b></h1>
-    <p>Using "b" tag.</p>
-    <h1><b>How to Italic any Text?</b></h1>
-    <p>Using "i" tag.</p>
-    <h1><b>How to Underlined any Text?</b></h1>
-    <p>Using "u" tag.</p>
+      <h1>HTML Assignment 1 Batch 6</h1>
+      <h1>Heading 1</h1>
+      <p>Heading 1 is made by using H1 tag</p>
+      <h2>Heading 2</h2>
+      <p>Heading 2 is made by using H2 tag</p>
+      <h3>Heading 3</h3>
+      <p>Heading 3 is made by using H3 tag</p>
+      <h4>Heading 4</h4>
+      <p>Heading 4 is made by using H4 tag</p>
+      <h5>Heading 5</h5>
+      <p>Heading 5 is made by using H5 tag</p>
+      <h6>Heading 6</h6>
+      <p>Heading 6 is made by using H6 tag</p>
+      <h1>
+        <u>Paragraph</u>
+      </h1>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Esse, magnam
+        dolorem debitis odio a tempora, quibusdam fugiat quae ad voluptates et
+        facilis odit nisi ipsa placeat eaque ex quos harum consectetur quis?
+        Praesentium reprehenderit totam, atque soluta, natus mollitia temporibus
+        ducimus quisquam repellat earum consequatur vitae ratione quisquam
+        eligendi repellat porro, quas dicta quos necessitatibus reiciendis illum
+        quod quo. Error quidem quisquam dolorum nostrum nemo rerum facere eius,
+        modi doloremque, officiis iste natus consequuntur. Molestias voluptas
+        quasi accusamus quisquam. Pariatur, quaerat?
+      </p>
+      <div class="font-style:italic">Italic Text</div>
+      <div class="font-style:bold">Bold Text</div>
+      <div class="font-style:underline">Underline Text</div>
+      <h1>
+        <b>How to Bold any Text?</b>
+      </h1>
+      <p>Using "b" tag.</p>
+      <h1>
+        <b>How to Italic any Text?</b>
+      </h1>
+      <p>Using "i" tag.</p>
+      <h1>
+        <b>How to Underlined any Text?</b>
+      </h1>
+      <p>Using "u" tag.</p>
     </div>
-  )
+  );
 }
